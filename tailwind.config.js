9feb3227513eb3fc6f23322fa3ffaff4d3541fef
dontliem1 <dontliem1@gmail.com>
2021-05-25 11:30:23 +0500

    Исправил ошибки в админке, добавил страницу со статьями

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,11 +20,11 @@ module.exports = {
       colors: colors,
     },
     screens: {
-      'sm': '40em',
-      'md': '48em',
-      'lg': '64em',
-      'xl': '80em',
-      '2xl': '96em'
+      'sm': '40rem',
+      'md': '48rem',
+      'lg': '64rem',
+      'xl': '80rem',
+      '2xl': '96rem'
     }
   }
 };
